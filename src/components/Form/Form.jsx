77a2022585cdc4176b919b21cbe38f05c6636f56
Nagiboshiro <vain335@gmail.com>
2022-11-28 21:49:41 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,7 +16,7 @@ const Form = () => {
         }
 
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
