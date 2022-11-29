157c9a99da60b7e962a70c342937557393b0b5d7
Nagiboshiro <vain335@gmail.com>
2022-11-29 10:33:28 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -26,7 +26,7 @@ const ProductList = () => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
-            queryId,
+            queryId: 'suka',
         }
 
 
